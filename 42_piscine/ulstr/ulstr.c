107ca0d655b2f049d69302a1769cdd7c502c69bf
elisio42 <elisiouiux@gmail.com>
2024-08-09 14:49:16 +0100

    ulstr

---
+++ 42_piscine/ulstr/ulstr.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+void	converte_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		converte_alpha(argv[1]);
+		write(1, "\n", 1);
+	}
+	else
+		write(1, "\n", 1);
+}
